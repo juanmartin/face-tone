30dd5d6192eb11983755938ec8056d83b4c05f64
Juan Martin <juanmartinsesali@gmail.com>
2022-04-28 22:27:13 -0300

    first

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+// import faceapi from "face-api.js";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="webcamFaceLandmarkDetection.html">face</a>
+`;
