9ab9ea6bb1cfaaba2fcc2956bb215976a79db528
Rodrigo López Dato <rodrigo@fossa.com>
2022-04-29 12:37:24 -0300

    Hello world!

--- src/faceDetectionControls.js
+++ src/faceDetectionControls.js
@@ -1,5 +1,9 @@
+import * as faceapi from "face-api.js";
+import { updateResults } from "./webcamFaceLandmarkDetection";
+import $ from "jquery";
+
 const SSD_MOBILENETV1 = 'ssd_mobilenetv1'
-const TINY_FACE_DETECTOR = 'tiny_face_detector'
+export const TINY_FACE_DETECTOR = 'tiny_face_detector'
 
 
 let selectedFaceDetector = TINY_FACE_DETECTOR
@@ -11,80 +15,62 @@ let minConfidence = 0.5
 let inputSize = 512
 let scoreThreshold = 0.5
 
-function getFaceDetectorOptions() {
+export function getFaceDetectorOptions() {
   return selectedFaceDetector === SSD_MOBILENETV1
     ? new faceapi.SsdMobilenetv1Options({ minConfidence })
     : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
 }
 
-function onIncreaseMinConfidence() {
+export function onIncreaseMinConfidence() {
   minConfidence = Math.min(faceapi.utils.round(minConfidence + 0.1), 1.0)
   $('#minConfidence').val(minConfidence)
   updateResults()
 }
 
-function onDecreaseMinConfidence() {
+export function onDecreaseMinConfidence() {
   minConfidence = Math.max(faceapi.utils.round(minConfidence - 0.1), 0.1)
   $('#minConfidence').val(minConfidence)
   updateResults()
 }
 
-function onInputSizeChanged(e) {
+export function onInputSizeChanged(e) {
   changeInputSize(e.target.value)
   updateResults()
 }
 
-function changeInputSize(size) {
+export function changeInputSize(size) {
   inputSize = parseInt(size)
 
   const inputSizeSelect = $('#inputSize')
   inputSizeSelect.val(inputSize)
-  inputSizeSelect.material_select()
+  // inputSizeSelect.material_select()
 }
 
-function onIncreaseScoreThreshold() {
+export function onIncreaseScoreThreshold() {
   scoreThreshold = Math.min(faceapi.utils.round(scoreThreshold + 0.1), 1.0)
   $('#scoreThreshold').val(scoreThreshold)
   updateResults()
 }
 
-function onDecreaseScoreThreshold() {
+export function onDecreaseScoreThreshold() {
   scoreThreshold = Math.max(faceapi.utils.round(scoreThreshold - 0.1), 0.1)
   $('#scoreThreshold').val(scoreThreshold)
   updateResults()
 }
 
-function onIncreaseMinFaceSize() {
-  minFaceSize = Math.min(faceapi.utils.round(minFaceSize + 20), 300)
-  $('#minFaceSize').val(minFaceSize)
+export function getCurrentFaceDetectionNet() {
+  return faceapi.nets.tinyFaceDetector;
 }
 
-function onDecreaseMinFaceSize() {
-  minFaceSize = Math.max(faceapi.utils.round(minFaceSize - 20), 50)
-  $('#minFaceSize').val(minFaceSize)
-}
-
-function getCurrentFaceDetectionNet() {
-  if (selectedFaceDetector === SSD_MOBILENETV1) {
-    return faceapi.nets.ssdMobilenetv1
-  }
-  if (selectedFaceDetector === TINY_FACE_DETECTOR) {
-    return faceapi.nets.tinyFaceDetector
-  }
-}
-
-function isFaceDetectionModelLoaded() {
+export function isFaceDetectionModelLoaded() {
   return !!getCurrentFaceDetectionNet().params
 }
 
-async function changeFaceDetector(detector) {
+export async function changeFaceDetector(detector) {
   ['#ssd_mobilenetv1_controls', '#tiny_face_detector_controls']
     .forEach(id => $(id).hide())
 
   selectedFaceDetector = detector
-  const faceDetectorSelect = $('#selectFaceDetector')
-  faceDetectorSelect.val(detector)
-  faceDetectorSelect.material_select()
 
   $('#loader').show()
   if (!isFaceDetectionModelLoaded()) {
@@ -102,11 +88,11 @@ async function onSelectedFaceDetectorChanged(e) {
   updateResults()
 }
 
-function initFaceDetectionControls() {
+export function initFaceDetectionControls() {
   const faceDetectorSelect = $('#selectFaceDetector')
   faceDetectorSelect.val(selectedFaceDetector)
   faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
-  faceDetectorSelect.material_select()
+  // faceDetectorSelect.material_select()
 
   const inputSizeSelect = $('#inputSize')
   inputSizeSelect.val(inputSize)
---
+++ src/webcamFaceLandmarkDetection.js
@@ -0,0 +1,67 @@
+import * as faceapi from "face-api.js";
+import { TINY_FACE_DETECTOR, isFaceDetectionModelLoaded, getFaceDetectorOptions, changeFaceDetector, changeInputSize } from "./faceDetectionControls";
+import $ from "jquery";
+
+    let forwardTimes = [];
+    let withBoxes = true;
+
+    export function onChangeHideBoundingBoxes(e) {
+      withBoxes = !$(e.target).prop("checked");
+    }
+
+    export function updateTimeStats(timeInMs) {
+      forwardTimes = [timeInMs].concat(forwardTimes).slice(0, 30);
+      const avgTimeInMs =
+        forwardTimes.reduce((total, t) => total + t) / forwardTimes.length;
+      $("#time").val(`${Math.round(avgTimeInMs)} ms`);
+      $("#fps").val(`${faceapi.utils.round(1000 / avgTimeInMs)}`);
+    }
+
+    export async function onPlay() {
+      const videoEl = $("#inputVideo").get(0);
+
+      if (videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
+        return setTimeout(() => onPlay());
+
+      const options = getFaceDetectorOptions();
+
+      const ts = Date.now();
+
+      const result = await faceapi
+        .detectSingleFace(videoEl, options)
+        .withFaceLandmarks();
+
+      updateTimeStats(Date.now() - ts);
+
+      if (result) {
+        const canvas = $("#overlay").get(0);
+        const dims = faceapi.matchDimensions(canvas, videoEl, true);
+        const resizedResult = faceapi.resizeResults(result, dims);
+
+        if (withBoxes) {
+          faceapi.draw.drawDetections(canvas, resizedResult);
+        }
+        faceapi.draw.drawFaceLandmarks(canvas, resizedResult);
+      }
+
+      setTimeout(() => onPlay());
+    }
+
+    export async function run() {
+      // load face detection and face landmark models
+      await changeFaceDetector(TINY_FACE_DETECTOR);
+      await faceapi.loadFaceLandmarkModel("/");
+      changeInputSize(224);
+
+      // try to access users webcam and stream the images
+      // to the video element
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+      const videoEl = $("#inputVideo").get(0);
+      videoEl.srcObject = stream;
+    }
+
+    export function updateResults() {}
+
+    $(document).ready(function () {
+      run();
+    });
